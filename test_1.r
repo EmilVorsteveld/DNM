8fea23726e53bdb981c7b2cb49b6662081aaeac1
Emil Vorsteveld <emil.vorsteveld@gmail.com>
2020-09-30 18:04:25 +0200

    Eerste probeersels met DNM data van Goldmann et al. 2016

---
+++ test_1.r
@@ -0,0 +1,176 @@
+library(dplyr)
+
+library(ggplot2)
+library(cowplot)
+
+setwd('/Users/emilvorsteveld/work/R/DNM') #set working directory
+data <- read.csv2("data.csv") #load data from csv file
+data[is.na(data)] = "DNM"
+
+pat <- 816
+nrow_total <- nrow(data)
+print(nrow_total)
+avg_variants <- nrow_total / pat
+
+
+#data %>% select(Chromosome, Start.position, End.position, Reference, Variant, parentOfOrigin)
+
+#First figure: number of variants from either the mother of the father
+paternal <- data %>% filter(parentOfOrigin == "father") #filter paternal variants from the data
+npaternal <- nrow(paternal) #count the number of paternal variants
+print(npaternal)
+
+maternal <- data %>% filter(parentOfOrigin == 'mother') #filter maternal variants from the data
+nmaternal <- nrow(maternal) #count the number of maternal variants
+print(nmaternal)
+
+parent <- c("Paternal", "Maternal") #concatenate paternal and maternal
+numbers <- c(npaternal, nmaternal) #concatenate number of pat and mat variants
+
+df <- data.frame(parent, numbers) #make a data frame from the numbers of variants and the parent of origin
+
+df_fig <- ggplot(data=df, aes(x=parent, y=numbers, fill=parent))+ #make a figure of the number of pat and mat variants
+    geom_bar(stat="identity")+
+    ggtitle("Whole genome")+ 
+    theme_classic()
+
+#Second figure: number of variant by type and parent of origin
+#First, the variants are filtered on type and parent of origin
+TGpaternal <- paternal %>% filter(Reference =="T", Variant=="G")
+nTGpaternal <- nrow(TGpaternal)
+
+CApaternal <- paternal %>% filter(Reference =="C", Variant=="A")
+nCApaternal <- nrow(CApaternal)
+
+TApaternal <- paternal %>% filter(Reference =="T", Variant=="A")
+nTApaternal <- nrow(TApaternal)
+
+TCpaternal <- paternal %>% filter(Reference =="T", Variant=="C")
+nTCpaternal <- nrow(TCpaternal)
+
+CTpaternal <- paternal %>% filter(Reference =="C", Variant=="T")
+nCTpaternal <- nrow(CTpaternal)
+
+CGpaternal <- paternal %>% filter(Reference =="C", Variant=="G")
+nCGpaternal <- nrow(CGpaternal)
+
+
+TGmaternal <- maternal %>% filter(Reference =="T", Variant=="G")
+nTGmaternal <- nrow(TGmaternal)
+
+CAmaternal <- maternal %>% filter(Reference =="C", Variant=="A")
+nCAmaternal <- nrow(CAmaternal)
+
+TAmaternal <- maternal %>% filter(Reference =="T", Variant=="A")
+nTAmaternal <- nrow(TAmaternal)
+
+TCmaternal <- maternal %>% filter(Reference =="T", Variant=="C")
+nTCmaternal <- nrow(TCmaternal)
+
+CTmaternal <- maternal %>% filter(Reference =="C", Variant=="T")
+nCTmaternal <- nrow(CTmaternal)
+
+CGmaternal <- maternal %>% filter(Reference =="C", Variant=="G")
+nCGmaternal <- nrow(CGmaternal)
+
+#Making a data frame, adding all the data together
+parent_var <- c("TG", "TG", "CA", "CA", "TA", "TA", "TC", "TC", "CT", "CT", "CG", "CG")
+
+numbers_var <- c(nTGpaternal, nTGmaternal, nCApaternal, nCAmaternal, nTApaternal, nTAmaternal, nTCpaternal, nTCmaternal, nCTpaternal, nCTmaternal, nCGpaternal,   nCGmaternal)
+
+origin_var <- c("paternal", "maternal", "paternal", "maternal", "paternal", "maternal", "paternal", "maternal", "paternal", "maternal", "paternal", "maternal")
+
+df_var <- data.frame(origin_var, parent_var, numbers_var)
+
+df_var_fig <- ggplot(data=df_var, aes(factor(parent_var), numbers_var, fill=origin_var))+ #Second figure
+    geom_bar(stat="identity", position=position_dodge())+
+    theme_classic()
+
+
+
+#Third figure: the number of variants from each chromosome
+# The for loop makes the names of the chromosomes and filters the data each time, retrieving the variants in each chrom., then the number is counted and added to a data frame together with the chrom. number. This is then plotted.
+nchr_total <- data.frame()
+for (i in c(1:22)){
+    n <- paste("chr", i, sep = "")
+    a <- data %>% filter(Chromosome == n)
+    b <- paste("n", n, sep = "")
+    c <- nrow(a)
+    nchr <- data.frame("Chromosome" = substr(n, 4 ,5), "Number of variants" = c)
+    print(nchr)
+    
+
+    nchr_total <- rbind(nchr_total, nchr)
+}
+print(nchr_total)
+
+nchr_total$Chromosome <- factor(nchr_total$Chromosome, levels = nchr_total$Chromosome)
+
+nchr_fig <- ggplot(data=nchr_total, aes(Chromosome, Number.of.variants, fill=Number.of.variants))+ #Third figure
+    geom_bar(stat="identity", position=position_dodge())+
+    ggtitle("Variants per chromsome")+
+    theme_classic()
+
+
+
+dnms <- data %>% filter(parentOfOrigin == "DNM")
+ndnms <- nrow(dnms)
+print(ndnms)
+
+ndnm_total <- data.frame()
+for (dnmi in c(1:22)){
+    dnmn <- paste("chr", dnmi, sep = "")
+    dnma <- dnms %>% filter(Chromosome == dnmn)
+    dnmb <- paste("dnmn", dnmn, sep = "")
+    dnmc <- nrow(dnma)
+    ndnm <- data.frame("Chromosome" = substr(dnmn, 4 ,5), "Number of variants" = dnmc)
+    print(ndnm)
+    
+    
+    ndnm_total <- rbind(ndnm_total, ndnm)
+}
+print(ndnm_total)
+
+ndnm_total$Chromosome <- factor(ndnm_total$Chromosome, levels = ndnm_total$Chromosome)
+
+ndnm_fig <- ggplot(data=ndnm_total, aes(Chromosome, Number.of.variants, fill=Number.of.variants))+ #Third figure
+    geom_bar(stat="identity", position=position_dodge())+
+    ggtitle("DNMs per chromsome")+
+    theme_classic()
+
+
+TGdnm <- dnms %>% filter(Reference =="T", Variant=="G")
+nTGdnm <- nrow(TGdnm)
+
+TCdnm <- dnms %>% filter(Reference =="T", Variant=="C")
+nTCdnm <- nrow(TCdnm)
+
+TAdnm <- dnms %>% filter(Reference =="T", Variant=="A")
+nTAdnm <- nrow(TAdnm)
+
+CTdnm <- dnms %>% filter(Reference =="C", Variant=="T")
+nCTdnm <- nrow(CTdnm)
+
+CGdnm <- dnms %>% filter(Reference =="C", Variant=="G")
+nCGdnm <- nrow(CGdnm)
+
+CAdnm <- dnms %>% filter(Reference =="C", Variant=="A")
+nCAdnm <- nrow(CAdnm)
+
+#Making a data frame, adding all the data together
+dnm_var <- c("TG", "CA", "TA", "TC", "CT", "CG")
+
+dnm_numbers_var <- c(nTGdnm, nCAdnm, nTAdnm, nTCdnm, nCTdnm, nCGdnm)
+
+df_var_dnm <- data.frame(dnm_var, dnm_numbers_var)
+
+df_var_dnm_fig <- ggplot(data=df_var_dnm, aes(dnm_var, dnm_numbers_var, fill=dnm_numbers_var))+ #Fourth figure
+    geom_bar(stat="identity", position=position_dodge())+
+    theme_classic()
+
+
+
+
+
+plot_grid(df_fig, df_var_fig, nchr_fig, ndnm_fig, df_var_dnm_fig) #Adding the figures into one image
+
